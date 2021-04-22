542732136ec50d66414a8e935e9ac1aa4a99da36
Serhii <serhii.nikiforenko@gmail.com>
2021-04-22 17:56:56 +0300

    Components in hooks, json-server

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { Categories, SortPopup, PizzaBlock } from '../components';
+import { useSelector, useDispatch } from 'react-redux';
 
-function Home({ items }) {
+import { Categories, SortPopup, PizzaBlock } from '../components';
+import { setCategory } from '../redux/actions/filters';
+
+const categoryNames = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закритые'];
+const sortItems = [
+  { name: 'популярности', type: 'popular' },
+  { name: 'цене', type: 'price' },
+  { name: 'алфавиту', type: 'alphabet' },
+];
+
+function Home() {
+  const dispatch = useDispatch();
+  const items = useSelector(({ pizzas }) => pizzas.items);
+
+  const onSelectCategory = React.useCallback((index) => {
+    dispatch(setCategory(index));
+  }, []);
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categories
-          onClickItem={(name) => console.log(name)}
-          items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закритые']}
-        />
+        <Categories onClickItem={onSelectCategory} items={categoryNames} />
         <SortPopup
           items={[
             { name: 'популярности', type: 'popular' },
@@ -19,9 +33,7 @@ function Home({ items }) {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items.map((obj) => (
-          <PizzaBlock key={obj.id} {...obj} />
-        ))}
+        {items && items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   );
